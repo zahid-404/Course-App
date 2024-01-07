2cd248eef6d975b6c9e8628b95e6c0e8c75dee62
Zahid <zahidmohammad495@gmai.com>
2024-01-07 10:43:02 +0530

    installing frontend dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  corePlugins: {
+    preflight: false,
+  },
+  important: '#root',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

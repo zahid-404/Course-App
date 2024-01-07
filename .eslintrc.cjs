05b723c7511dbf4780394136c12ab180d257445a
Zahid <zahidmohammad495@gmai.com>
2024-01-07 20:14:17 +0530

    configure eslint for backend code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,24 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
+  env: { node: true, es2020: true },
+  extends: ['eslint:recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['express'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    // Global rules for the entire project
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'no-console': 'off',
   },
-}
+  overrides: [
+    {
+      files: ['server/**/*.js'], // Apply rules only to files inside the server folder
+      extends: ['plugin:express/recommended'], // Extend with express-specific rules
+      rules: {
+        // Server-specific rules can be added or overridden here
+        'express/no-unused-vars': 'error',
+      },
+    },
+  ],
+};
